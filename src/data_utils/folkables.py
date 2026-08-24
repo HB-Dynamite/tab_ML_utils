1234ac182ad38d30334402b57c75c9a87e5071ba
HB-Dynamite <emil.bohlen@gmail.com>
2026-08-24 16:40:53 +0200

    update the prepare data function

--- src/data_utils/folkables.py
+++ src/data_utils/folkables.py
@@ -3,7 +3,7 @@ from folktables.acs import adult_filter
 import pandas as pd
 import numpy as np
 
-CSIncomeRegression = BasicProblem(
+ACSIncomeRegression = BasicProblem(
     features=[
         "AGEP",  # age
         "SEX",   # sex
@@ -91,13 +91,31 @@ MAR_MAP = {
     }
 
 
-def prepare_data(state, n=10_000):
-    source = ACSDataSource(survey_year="2018", horizon="1-Year", survey="person")
+def prepare_data(state, n=10_000, income_threshold=300_000, year="2018"):
+    """
+
+    Prepare the ACS data for a given state and year, filtering by income threshold and sampling n rows.
+    
+    Parameters
+    ----------
+    param state: The state to filter the data by (e.g., "CA" for California).
+    
+    param n: The number of rows to sample from the filtered data.
+    
+    param income_threshold: The maximum income to filter the data by.
+    
+    param year: The year of the ACS data to use (e.g., "2018"). Available years are 2018,....
+    
+    Returns
+    -------
+    return: A pandas DataFrame containing the prepared data.
+    """
+    source = ACSDataSource(survey_year=str(year), horizon="1-Year", survey="person")
     data = source.get_data(states=[state], download=True)
     X, y, _ = ACSIncomeRegression.df_to_pandas(data)
     df = pd.concat([X, y], axis=1)
 
-    df = df[df["PINCP"] < 300_000].sample(n=n, random_state=42)
+    df = df[df["PINCP"] < income_threshold].sample(n=n, random_state=42)
     df["INDP"] = df["INDP"].apply(map_industry)
     df["SCHL"] = df["SCHL"].apply(map_education)
     df["ESR"] = df["ESR"].map(ESR_MAP)
